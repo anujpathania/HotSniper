096c17089581332df03c7ca307dfab627cb72714
Martin Rapp <martin.rapp@kit.edu>
2020-10-18 10:59:53 +0200

    fix CI pipeline

--- ci/build_sniper.sh
+++ ci/build_sniper.sh
@@ -1,6 +1,7 @@
 set -e
 
 # PIN
+wget https://software.intel.com/content/dam/develop/external/us/en/protected/pinplay-drdebug-3.2-pin-3.2-81205-gcc-linux.tar.gz
 tar -xvzf pinplay-drdebug-3.2-pin-3.2-81205-gcc-linux.tar.gz
 mv pinplay-drdebug-3.2-pin-3.2-81205-gcc-linux pin_kit
 
